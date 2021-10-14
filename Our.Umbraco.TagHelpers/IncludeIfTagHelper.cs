7b616f646bf9fa0558318fb6a65148a9fa8a2fee
Warren Buckley <warren@umbraco.com>
2021-10-14 13:31:46 +0100

    If TagHelper

---
+++ Our.Umbraco.TagHelpers/IncludeIfTagHelper.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Our.Umbraco.TagHelpers
+{
+    /// <summary>
+    /// Apply the attribute our-if to any HTML DOM element for it to be included in the page
+    /// as long as the value inside the attribute evaluates to true
+    /// </summary>
+    [HtmlTargetElement("*", Attributes = "our-if")]
+    public class IncludeIfTagHelper : TagHelper
+    {
+        /// <summary>
+        /// A boolean expression
+        /// If it evaluates to true then it will be kept in the page
+        /// </summary>
+        [HtmlAttributeName("our-if")]
+        public bool? Predicate { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (!Predicate.HasValue || !Predicate.Value)
+            {
+                output.SuppressOutput();
+            }
+        }
+    }
+}
