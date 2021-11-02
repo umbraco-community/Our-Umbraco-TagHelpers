76d54f0d98ea19bd96143a91475916f9d961a0b5
Paul Seal <prjseal@gmail.com>
2021-11-02 14:17:26 +0000

    - got tests working

--- Our.Umbraco.TagHelpers.Tests/IncludeIfTagHelperTests.cs
+++ Our.Umbraco.TagHelpers.Tests/IncludeIfTagHelperTests.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Our.Umbraco.TagHelpers.Tests
 {
-    public class Tests
+    public class IncludeIfTagHelperTests
     {
         [Test]
         public async Task Given_True_Value_Return_Contents()
@@ -19,20 +19,51 @@ namespace Our.Umbraco.TagHelpers.Tests
             var tagHelperOutput = GetTagHelperOutput(
                 attributes: new TagHelperAttributeList(),
                 childContent: childContent);
+            tagHelperOutput.Content.SetContent(childContent);
+
             var tagHelper = new IncludeIfTagHelper();
+            tagHelper.Predicate = true;
 
             // Act
             await tagHelper.ProcessAsync(tagHelperContext, tagHelperOutput);
 
+            var content = tagHelperOutput.Content.GetContent();
+            Console.WriteLine("Value:" + content);
+
             // Assert
             Assert.AreEqual(childContent, tagHelperOutput.Content.GetContent());
+        }
+
+        [Test]
+        public async Task Given_False_Value_Return_Empty()
+        {
+            // Arrange
+            var id = "unique-id";
+            var childContent = "original-child-content";
+            var tagHelperContext = GetTagHelperContext(id);
+            var tagHelperOutput = GetTagHelperOutput(
+                attributes: new TagHelperAttributeList(),
+                childContent: childContent);
+            tagHelperOutput.Content.SetContent(childContent);
+
+            var tagHelper = new IncludeIfTagHelper();
+            tagHelper.Predicate = false;
+
+            // Act
+            await tagHelper.ProcessAsync(tagHelperContext, tagHelperOutput);
+
+            var content = tagHelperOutput.Content.GetContent();
+            Console.WriteLine("Value:" + content);
+
+            // Assert
+            Assert.AreEqual(string.Empty, tagHelperOutput.Content.GetContent());
         }
 
         private static TagHelperContext GetTagHelperContext(string id = "testid")
         {
             return new TagHelperContext(
                 tagName: "p",
-                allAttributes: new TagHelperAttributeList() { new TagHelperAttribute("our-if", true) },
+                allAttributes: new TagHelperAttributeList(),
                 items: new Dictionary<object, object>(),
                 uniqueId: id);
         }
@@ -50,8 +81,8 @@ namespace Our.Umbraco.TagHelpers.Tests
                 getChildContentAsync: (useCachedResult, encoder) =>
                 {
                     var tagHelperContent = new DefaultTagHelperContent();
-                    tagHelperContent.SetHtmlContent(childContent);
-                    return Task.FromResult<TagHelperContent>(tagHelperContent);
+                    var content = tagHelperContent.SetHtmlContent(childContent);
+                    return Task.FromResult<TagHelperContent>(content);
                 });
         }
     }
