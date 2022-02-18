81d95b13ebfd87d50f7feb92461ceb801f8f9ab7
Warren Buckley <warren@umbraco.com>
2022-02-18 10:52:33 +0000

    Update Our.Umbraco.TagHelpers/IsActivePageTagHelper.cs
    Co-authored-by: Ronald Barendse <ronald@barend.se>

--- Our.Umbraco.TagHelpers/IsActivePageTagHelper.cs
+++ Our.Umbraco.TagHelpers/IsActivePageTagHelper.cs
@@ -36,7 +36,7 @@ namespace Our.Umbraco.TagHelpers
         {
             // Remove the attribute 
             // We don't want it in the markup we send down to the page
-            output.Attributes.RemoveAll("our-is-active-page");
+            output.Attributes.RemoveAll(tagHelperAttributeName);
 
             var ctx = _umbracoContextAccessor.GetRequiredUmbracoContext();
 
