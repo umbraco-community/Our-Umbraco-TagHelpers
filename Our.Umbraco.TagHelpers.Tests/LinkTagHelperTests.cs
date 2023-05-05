9da891e44d3021386681642de1eaf69335ce6165
Matthew Wise <6782865+Matthew-Wise@users.noreply.github.com>
2023-05-05 14:24:40 +0100

    refactored link tests so they are more split

--- Our.Umbraco.TagHelpers.Tests/LinkTagHelperTests.cs
+++ Our.Umbraco.TagHelpers.Tests/LinkTagHelperTests.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Razor.TagHelpers;
@@ -11,43 +10,105 @@ namespace Our.Umbraco.TagHelpers.Tests
 {
     public class LinkTagHelperTests
     {
+        private static readonly Link _onSiteLink = new() { Url = "/", Name = "example" };
 
-        [TestCaseSource(nameof(TestCases))]
-        public async Task LinkHelperTests(LinkTagHelper tagHelper, string childContent, string expectedOutput)
+        private static readonly Link _externalLink = new() { Url = "/", Name = "example", Target = "_blank", Type = LinkType.External };
+
+        private TagHelperContext _tagHelperContext;
+
+        [SetUp]
+        public void SetUp()
         {
-            var tagHelperContext = TestContextHelpers.GetTagHelperContext("link");
-            var output = TestContextHelpers.GetTagHelperOutput("our-link",
-                attributes: new TagHelperAttributeList(),
-                childContent: childContent);
+            _tagHelperContext = TestContextHelpers.GetTagHelperContext("link");
+        }
+
+        [TestCase("", "example")]
+        [TestCase(null, "example")]
+        [TestCase("content", "content")]
+        public async Task Internal_Link_Renders_AnchorAroundContentOrLinkName(string childContent, string expectedContent)
+        {
+            var output = TestContextHelpers.GetTagHelperOutput("our-link", childContent: childContent);
             output.Content.SetContent(childContent);
 
-            await tagHelper.ProcessAsync(tagHelperContext, output);
+            LinkTagHelper tagHelper = new() { Link = _onSiteLink };
+
+            var markup = await GetMarkupAsync(tagHelper, output);
+            Assert.AreEqual($"<a href=\"/\">{expectedContent}</a>", markup);
+        }
+
+        [TestCase("", "example")]
+        [TestCase(null, "example")]
+        [TestCase("content", "content")]
+        public async Task External_Link_Renders_AnchorAroundContentOrLinkName(string childContent, string expectedContent)
+        {
+            var output = TestContextHelpers.GetTagHelperOutput("our-link", childContent: childContent);
+            output.Content.SetContent(childContent);
+
+            LinkTagHelper tagHelper = new() { Link = _externalLink };
+
+            var markup = await GetMarkupAsync(tagHelper, output);
+            Assert.AreEqual($"<a href=\"/\" target=\"_blank\" rel=\"noopener\">{expectedContent}</a>", markup);
+        }
+
+        [Test]
+        public async Task NoUrl_WithoutFallback_RendersNothing()
+        {
+            var output = TestContextHelpers.GetTagHelperOutput("our-link", childContent: string.Empty);
+            output.Content.SetContent(string.Empty);
+
+            LinkTagHelper tagHelper = new() { Link = new() };
+
+            var markup = await GetMarkupAsync(tagHelper, output);
+            Assert.AreEqual(string.Empty, markup);
+        }
+
+        [Test]
+        public async Task Null_Link_WithoutFallback_RendersNothing()
+        {
+            var output = TestContextHelpers.GetTagHelperOutput("our-link", childContent: string.Empty);
+            output.Content.SetContent(string.Empty);
+
+            LinkTagHelper tagHelper = new() { Link = null };
+
+            var markup = await GetMarkupAsync(tagHelper, output);
+            Assert.AreEqual(string.Empty, markup);
+        }
+
+        [TestCase("", "")]
+        [TestCase(null, "")]
+        [TestCase("content", "content")]
+        public async Task Null_Link_WithFallback_NoElement_RendersContent(string childContent, string expectedContent)
+        {
+            var output = TestContextHelpers.GetTagHelperOutput("our-link", childContent: childContent);
+            output.Content.SetContent(childContent);
+
+            LinkTagHelper tagHelper = new() { Link = null, Fallback = true };
+
+            var markup = await GetMarkupAsync(tagHelper, output);
+            Assert.AreEqual(expectedContent, markup);
+        }
+
+        [TestCase("", "")]
+        [TestCase(null, "")]
+        [TestCase("content", "<div>content</div>")]
+        public async Task Null_Link_WithFallback_AndElement_RendersContent(string childContent, string expectedContent)
+        {
+            var output = TestContextHelpers.GetTagHelperOutput("our-link", childContent: childContent);
+            output.Content.SetContent(childContent);
+
+            LinkTagHelper tagHelper = new() { Link = null, Fallback = true, FallbackElement = "div" };
+
+            var markup = await GetMarkupAsync(tagHelper, output);
+            Assert.AreEqual(expectedContent, markup);
+        }
+
+        private async Task<string> GetMarkupAsync(LinkTagHelper tagHelper, TagHelperOutput output)
+        {
+            await tagHelper.ProcessAsync(_tagHelperContext, output);
 
             using var txtWriter = new StringWriter();
             output.WriteTo(txtWriter, HtmlEncoder.Default);
-            Assert.AreEqual(expectedOutput, txtWriter.ToString());
-        }
-
-        private static IEnumerable<TestCaseData> TestCases()
-        {
-            var onSiteLink = new Link { Url = "/", Name = "example" };
-            var externalLink = new Link { Url = "/", Name = "example", Target = "_blank", Type = LinkType.External };
-
-            return new[] {
-                new TestCaseData(new LinkTagHelper(), "content", string.Empty).SetName("No content rendered if Link is null"),
-
-                new TestCaseData(new LinkTagHelper { Link = onSiteLink},null,"<a href=\"/\">example</a>").SetName("Internal link is rendered with no content"),
-                new TestCaseData(new LinkTagHelper { Link = onSiteLink},"content","<a href=\"/\">content</a>").SetName("Internal link is rendered with content"),
-
-                new TestCaseData(new LinkTagHelper { Link = externalLink},null,"<a href=\"/\" target=\"_blank\" rel=\"noopener\">example</a>").SetName("External link with target is rendered with no content"),
-                new TestCaseData(new LinkTagHelper { Link = externalLink},"content","<a href=\"/\" target=\"_blank\" rel=\"noopener\">content</a>").SetName("External link with target is rendered with content"),
-
-
-                new TestCaseData(new LinkTagHelper() { Fallback = true }, "", "").SetName("Fallback with no content"),
-                new TestCaseData(new LinkTagHelper() { Fallback = true }, "content", "content").SetName("Fallback with only content"),
-                new TestCaseData(new LinkTagHelper() { Fallback = true, FallbackElement = "div" }, "content", "<div>content</div>")
-                .SetName("Fallback with fallback element and content")
-            };
+            return txtWriter.ToString();
         }
     }
 }
